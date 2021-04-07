5cf1eab760b30c1a5a6e81e1e772e2dafd7b7c44
scarzdz <scar.zdz@gmail.com>
2021-04-07 13:02:53 +0300

    Remove redunant code

--- src/connection.ts
+++ src/connection.ts
@@ -3,15 +3,4 @@ import mysqlconfig from './mysqlconfig'
 
 const pool = mysql.createPool(mysqlconfig);
 
-
-let connection = mysql.createConnection(mysqlconfig)
-
-connection.connect(function (err) {
-    if (err) {
-        return console.error('error: ' + err.message)
-    }
-
-    console.log('Connected to the MySQL server.')
-})
-
 export const db = pool
