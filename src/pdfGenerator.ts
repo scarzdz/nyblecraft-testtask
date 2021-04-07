00d77c479ec0d3a892edf6bea4bdf6271b7a8909
scarzdz <scar.zdz@gmail.com>
2021-04-07 11:46:45 +0300

    Use custom font for pdf generator
    Used OpenSans because default fonts don't support non-ASCII symbols.
    Font converted to Base64.

--- src/pdfGenerator.ts
+++ src/pdfGenerator.ts
@@ -1,4 +1,11 @@
 import { jsPDF } from 'jspdf'
+import font from './font/OpenSans-normal'
+
+const callAddFont = function (this: any) {
+    this.addFileToVFS('OpenSans-normal.ttf', font)
+    this.addFont('OpenSans-normal.ttf', 'OpenSans', 'normal')
+}
+jsPDF.API.events.push(['addFonts', callAddFont])
 
 const binaryToDataURL = (imageData: Buffer) => {
     return 'data:image/jpeg;base64,' + imageData.toString('base64')
@@ -8,6 +15,7 @@ const generatePDF = (firstName: string, lastName: string, imageData: Buffer) => 
     const image = binaryToDataURL(imageData)
     const doc = new jsPDF()
 
+    doc.setFont('OpenSans', 'normal')
     const displayName = firstName + ' ' + lastName
     doc.text(displayName, 20, 30)
 
