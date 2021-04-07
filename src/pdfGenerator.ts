2bf1fdbe930682af62602d3c395ede464a5334e0
scarzdz <scar.zdz@gmail.com>
2021-04-07 15:28:20 +0300

    pdfGenerator now preserves image aspect ratio

--- src/pdfGenerator.ts
+++ src/pdfGenerator.ts
@@ -1,5 +1,25 @@
 import { jsPDF } from 'jspdf'
+import sizeOf from "image-size"
 import "./font/OpenSans-normal"
+
+const MAX_SIZE = 100
+
+const getImageSize = (image: Buffer) => {
+    const dimensions = sizeOf(image)
+    let sizeX = 0
+    let sizeY = 0
+
+    if (dimensions.height && dimensions.width) {
+        if (dimensions.height >= dimensions.width) {
+            sizeY = MAX_SIZE
+            sizeX = MAX_SIZE * dimensions.width / dimensions.height
+        } else {
+            sizeX = MAX_SIZE
+            sizeY = MAX_SIZE * dimensions.height / dimensions.width
+        }
+    }
+    return { x: sizeX, y: sizeY }
+}
 
 const generatePDF = (firstName: string, lastName: string, image: Buffer) => {
     const doc = new jsPDF()
@@ -7,9 +27,12 @@ const generatePDF = (firstName: string, lastName: string, image: Buffer) => {
     doc.setFont('OpenSans', 'normal')
     const displayName = firstName + ' ' + lastName
     doc.text(displayName, 20, 30)
+
+    const size = getImageSize(image)
     // according to jsPDF documentation, second parameter is used only 
     // if automatic type recognition fails, so it is safe to use PNG or WEBP
-    doc.addImage(image, 'JPEG', 20, 40, 50, 50)
+    doc.addImage(image, 'JPEG', 20, 40, size.x, size.y)
+
 
     return doc.output('arraybuffer')
 }
